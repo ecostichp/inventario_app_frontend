2ac2747d05defb6aadbe70c91fda9a3bcc75d0b5
gretel <lacasadelcarpintero29@gmail>
2022-11-08 14:13:47 -0700

    se agrega login en el home y el punto ENV

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,46 @@
-export default function Home() {
+import { useEffect, useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
+
+
+export default function Home( { }) {
+  
+  
+  const [usuario, setUsuario] = useState()
+  const [contraseña, setContarseña] = useState()
+ 
+
+  useEffect (() => {
+
+  })
+
+
+  const click_submit = (e) => {
+    e.preventDefault()
+    console.log(e.target.login_usuario.value)
+    console.log(e.target.login_contraseña.value)
+  } 
+
   return (
-      <main>
-       wep
-      </main>
+    
+    <main style={{display:"flex", justifyContent: "center"}}>
+      
+    
+    <div>
+      <Form onSubmit={click_submit}>
+
+        <FloatingLabel controlId="login_usuario" name ="login_usuario" value = {usuario} label="Usuario" className="m-3">
+
+          <Form.Control type="text"  placeholder="Usuario" />
+        </FloatingLabel>
+        <FloatingLabel controlId="login_contraseña" name = "login_contraseña"  value= {contraseña} label="Contaseña" className="m-3">
+          <Form.Control type="password" placeholder="Contaseña" />
+        </FloatingLabel>
+        <Button className="m-3" variant="primary" type='submit'>Entrar</Button>
+      </Form>
+    </div>
+ 
+    </main>
   )
 }
