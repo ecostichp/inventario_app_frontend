75e620d0577f4c7accab334003cae5b0d1b27f4d
gretel <lacasadelcarpintero29@gmail>
2022-11-09 18:01:08 -0700

    Se agrega Login2 copia Finder

---
+++ components/PasswordToggle.js
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+
+const PasswordToggle = ({
+  size,
+  light,
+  className,
+  style,
+  ...props
+}) => {
+
+  const inputSize = size ? ` form-control-${size}` : '',
+        isLight = light ? ' form-control-light' : '',
+        extraClass = className ? ` ${className}` : ''
+
+  const [show, setShow] = useState(false)
+
+  return (
+    <div className={`password-toggle${extraClass}`} style={style}>
+      <input
+        {...props}
+        type={show ? 'text' : 'password'}
+        className={`form-control${inputSize}${isLight}`}
+      />
+      <label className='password-toggle-btn' aria-label='Show/hide password'>
+        <span className='password-toggle-indicator'></span>
+      </label>
+    </div>
+  )
+}
+
+export default PasswordToggle
---
+++ pages/copyFinder.jsx
@@ -0,0 +1,58 @@
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Image from 'react-bootstrap/Image'
+import Link from 'next/link'
+import PasswordToggle from '../components/PasswordToggle'
+import { useState } from 'react';
+
+
+export default function LoginFinder () {
+
+  // Form validation
+  const [validated, setValidated] = useState(false)
+  const handleSubmit = (event) => {
+    const form = event.currentTarget
+    if (form.checkValidity() === false) {
+      event.preventDefault()
+      event.stopPropagation()
+    }
+    setValidated(true);
+  }
+    
+    
+    return(
+        <div className='container-fluid d-flex h-100 align-items-center justify-content-center py-4 py-sm-5 position-absolute top-50 start-50 translate-middle' style={{backgroundColor: "#e8e8e8"}} >
+        <div className='card card-body rounded-5' style={{maxWidth: '940px', borderColor: "#ced4da"}}> 
+            <div className='row mx-0 align-items-center '>
+              <div className='col-md-6 border-end-md p-2 p-sm-5'>
+                  <h2 className='h3 mb-4 mb-sm-5 text-center'>Bienvenido<br />Inventario San José 2022</h2>
+                    <div className='d-flex justify-content-center'>
+                      <Image src='Logo2021.png' width={300} alt='Logo' />
+                    </div>
+              </div>
+              <div className='col-md-6 px-2 pt-2 pb-4 px-sm-5 pb-sm-5 pt-md-5'>
+                    
+                <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                  <Form.Group controlId='si-email' className='mb-4'>
+                    <Form.Label>Usuario</Form.Label>
+                    <Form.Control
+                      type='email'
+                      placeholder='Ingresa tu Usuario'
+                      required
+                    />
+                  </Form.Group>
+                  <Form.Group className='mb-4'>
+                    <div className='d-flex align-items-center justify-content-between mb-2'>
+                      <Form.Label htmlFor='si-password' className='mb-0'>Contraseña</Form.Label>
+                    </div>
+                    <PasswordToggle id='si-password' placeholder='Ingresa tu Contraseña' required />
+                  </Form.Group>
+                  <Button type='submit' size='lg' variant='primary w-100'>Enviar</Button>
+                </Form>
+              </div>
+            </div>  
+          </div>  
+        </div>
+
+    )
+}
